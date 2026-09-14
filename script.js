491594ed8c0462e5610eafb0193adb16887d1827
BonnieS <191048@cloud.rangitoto.school.nz>
2026-09-14 21:14:36 +0000

    testing

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
 document.getElementById('calc-btn').addEventListener('click', calculateWave);
 document.getElementById('clear-btn').addEventListener('click', resetCalc);
 
-function calculateWave(){
-let vText = document.getElementById('v').value;
-let fText = document.getElementById('f').value;
-let wText = document.getElementById('w').value;
-let result = document.getElementById('Result');
+function calculateWave() {
+  let vText = document.getElementById('v').value;
+  let fText = document.getElementById('f').value;
+  let wText = document.getElementById('w').value;
+  let result = document.getElementById('result'); // Fixed: lowercase 'result' to match HTML
 
-let v = Number(vText);
-let f = Number(fText);
-let w = Number(wText);
+  let v = Number(vText);
+  let f = Number(fText);
+  let w = Number(wText);
 
-if (vText == " " && fText != " " && wText != " ") {
+  // Checks for empty inputs ("") instead of spaces (" ")
+  if (vText == "" && fText != "" && wText != "") {
     let ans = f * w;
     document.getElementById('v').value = ans;
     result.innerText = "Calculated Wave Speed = " + ans + " m/s";
-}else if (vText != " " && fText != " " && wText == " ") {
+
+  } else if (vText != "" && fText != "" && wText == "") {
     let ans = v / f;
     document.getElementById('w').value = ans;
     result.innerText = "Calculated Wavelength = " + ans + " m";
-}else if (vText != " " && fText == " " && wText != " " ) {
-    let ans = v / f;
-    document.getElementById('w').value = ans;
-    result.innerText = "Calculated Wavelength = " + ans + " m";
-}else {
-    // If the user didn't fill in exactly 2 boxes
+
+  } else if (vText != "" && fText == "" && wText != "") {
+    // Fixed: Calculates frequency (f = v / w) and updates field 'f'
+    let ans = v / w;
+    document.getElementById('f').value = ans;
+    result.innerText = "Calculated Frequency = " + ans + " Hz";
+
+  } else {
     result.innerText = "Please enter exactly TWO values.";
   }
 }
-function resetCalc(){
-    document.getElementById('v').value = "";
+
+function resetCalc() {
+  document.getElementById('v').value = "";
   document.getElementById('f').value = "";
   document.getElementById('w').value = "";
   document.getElementById('result').innerText = "";
